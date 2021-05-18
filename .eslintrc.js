3f2d79dd0a9926487648a44e41e5d64ec000ae53
itouuuuuuuuu <mfito.13@gmail.com>
2021-05-18 14:19:05 +0900

    add linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,40 @@ module.exports = {
     browser: true,
     node: true
   },
+  parserOptions: {
+  },
   extends: [
     '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended'
+    "eslint:recommended",
+    "plugin:vue/recommended"
   ],
   plugins: [
+    'vue'
   ],
-  // add your custom rules here
-  rules: {}
+  rules: {
+    "object-curly-spacing": [2, "always"],
+    "space-before-blocks": "error",
+    "space-infix-ops": "error",
+    "space-before-function-paren": ["error", "never"],
+    "keyword-spacing": ["error", { "overrides": {
+      "if": { "after": false },
+      "else": { "after": true },
+      "switch": { "after": false },
+      "for": { "after": false },
+      "while": { "after": false }
+    } }],
+    "no-empty-pattern": "off",
+    "no-return-await": "off",
+    "indent": [2, 2],
+    "semi": "off",
+    "@typescript-eslint/semi": ["error"],
+    "curly": ["error", "multi-line"],
+    "eqeqeq": "error",
+    "no-trailing-spaces": "error",
+    "block-spacing": ["error", "always"],
+    "brace-style": ["error", "1tbs", { "allowSingleLine": false }],
+    "no-multi-spaces": "error",
+    "no-multiple-empty-lines": [1, { "max": 1 }],
+    "comma-spacing": [2, { "before": false, "after": true }]
+  }
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -51,6 +51,15 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    transpile: [/^element-ui/]
-  }
+    transpile: [/^element-ui/],
+    extend(config, ctx) {
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: "pre",
+          test: /\.(js|vue)$/,
+          loader: "eslint-loader",
+          exclude: /(node_modules)/
+        })
+      }
+    }}
 }
